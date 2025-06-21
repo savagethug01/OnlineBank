75bd42dec8fe8758a88e0f7d8b232ad7017b208d
savagethug01 <blueharry39@gmail.com>
2025-06-21 12:52:59 -0700

    Update manage.py
    Save

--- wealthbridge/manage.py
+++ wealthbridge/manage.py
@@ -2,16 +2,17 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.conf import settings
-
-if os.environ.get("DJANGO_SUPPRESS_PROMPTS", "false").lower() == "true":
-    from django.db.migrations import questioner
-    questioner.default_questioner = questioner.NonInteractiveQuestioner()
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wealthbridge.settings')
+
+    # Disable interactive prompts in non-interactive environments
+    if os.environ.get("DJANGO_SUPPRESS_PROMPTS", "false").lower() == "true":
+        from django.db.migrations import questioner
+        questioner.default_questioner = questioner.NonInteractiveMigrationQuestioner()
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,6 +21,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
