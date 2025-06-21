d0f6987a838b14197910803ede5fd17fc5183e81
savagethug01 <blueharry39@gmail.com>
2025-06-21 11:30:59 -0700

    Update utilis.py
    sAve

--- wealthbridge/bank_app/utilis.py
+++ wealthbridge/bank_app/utilis.py
@@ -3,17 +3,17 @@ from .forms import *
 from .models import *
 
 # utils.py
-def validate_otp(input_otp, user_profile):
+def validate_bic(input_bic, user_profile):
     # Compare the input OTP with the OTP stored in the user profile
-    return input_otp == user_profile.otp_code
+    return input_bic == user_profile.bic_code
 
 def validate_imf(input_imf, user_profile):
     # Compare the input OTP with the OTP stored in the user profile
     return input_imf == user_profile.imf_code
 
-def validate_aml(input_aml, user_profile):
+def validate_tax(input_tax, user_profile):
     # Compare the input OTP with the OTP stored in the user profile
-    return input_aml == user_profile.aml_code
+    return input_tax == user_profile.tax_code
 
 def validate_tac(input_tac, user_profile):
     # Compare the input OTP with the OTP stored in the user profile
